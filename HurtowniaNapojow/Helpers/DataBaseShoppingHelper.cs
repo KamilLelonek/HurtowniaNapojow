417a6d7887cbae5f4d0cf57815a4d47330cbd4e6
adasm <adasm@live.com>
2014-01-03 12:14:04 +0100

    Extended ShoppingHelper

--- HurtowniaNapojow/Helpers/DataBaseShoppingHelper.cs
+++ HurtowniaNapojow/Helpers/DataBaseShoppingHelper.cs
@@ -40,6 +40,13 @@ namespace HurtowniaNapojow.Helpers
                 sum + DataBaseProductHelper.GetProductProfit(customerProduct));
         }
 
+        public static float CalculateShoppingPrice(HurtowniaNapojowDataSet.ZakupyKlientaRow shopping)
+        {
+            var customerProducts = DataBaseProductHelper.GetProductsForShopping(shopping);
+            return (float)customerProducts.Aggregate(.0, (sum, customerProduct) =>
+                sum + customerProduct.Kwota);
+        }
+
         internal static object DeleteShoppingRow(System.Data.DataRow dataRow)
         {
             throw new System.NotImplementedException();
