8459efd766fbb8674ed11b8106a048c6e9c75c10
Squixy Sln <squixy.sln@hotmail.com>
2014-01-03 12:36:35 +0100

    Added waiting indicator

--- HurtowniaNapojow/Utils/WindowsPlugins.cs
+++ HurtowniaNapojow/Utils/WindowsPlugins.cs
@@ -1,9 +1,14 @@
-﻿namespace System.Windows
+﻿using System.Windows.Input;
+
+namespace System.Windows
 {
     public static class WindowsPlugins
     {
         public static void OpenWindow(this Window window, Window newWindow, bool closePrevious = true, bool blockPrevious = false)
         {
+            window.Cursor = Cursors.Wait;
+            newWindow.Loaded += (sender, args) => newWindow.Cursor = Cursors.Arrow;
+
             if (blockPrevious)
             {
                 newWindow.ShowDialog();
