600861b1e04ff7a7e271627f1cfe372095317df7
adasm <adasm@live.com>
2014-01-04 18:08:42 +0100

    Updated Shopping CRUD

---
+++ HurtowniaNapojow/Helpers/CustomerShopping.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HurtowniaNapojow.Database;
+
+namespace HurtowniaNapojow.Helpers
+{
+    public class CustomerShopping
+    {
+        public int Id { get; set; }
+        public String CustomerName { get; set; }
+        public String EmployeeName { get; set; }
+        public int ProductsCount { get; set; }
+        public int ProductsTotalAmount { get; set; }
+        public float Price { get; set; }
+
+        public String Date { get; set; }
+
+
+        public HurtowniaNapojowDataSet.KlienciRow       _customerRow;
+        public HurtowniaNapojowDataSet.PracownicyRow    _employeeRow;
+        public HurtowniaNapojowDataSet.ZakupyKlientaRow _shoppingRow;
+
+        public CustomerShopping(HurtowniaNapojowDataSet.KlienciRow customerRow, HurtowniaNapojowDataSet.ZakupyKlientaRow shoppingRow)
+        {
+            _customerRow = customerRow;
+            _shoppingRow = shoppingRow;
+            _employeeRow = DataBaseEmployeeHelper.GetEmployeesData().First(e => e.Identyfikator == shoppingRow.id_pracownika);
+
+            Id = _shoppingRow.Identyfikator;
+            CustomerName = _customerRow.NazwaKlienta;
+            EmployeeName = _employeeRow.Nazwisko + " " + _employeeRow.Imię;
+            Date = _shoppingRow.DataZłożenia.ToShortDateString();
+            ProductsCount = DataBaseShoppingHelper.CalculateShoppingProductsCount(_shoppingRow);
+            ProductsTotalAmount = DataBaseShoppingHelper.CalculateShoppingProductsTotalAmount(_shoppingRow);
+            Price = DataBaseShoppingHelper.CalculateShoppingPrice(_shoppingRow);
+        }
+
+        public static IEnumerable<CustomerShopping> GetCustomerShoppings(HurtowniaNapojowDataSet.KlienciRow customerRow)
+        {
+            var shoppings = DataBaseShoppingHelper.GetShoppingForCustomer(customerRow);
+            return shoppings.Select(shopping => new CustomerShopping(customerRow, shopping));
+        }
+    }
+}
