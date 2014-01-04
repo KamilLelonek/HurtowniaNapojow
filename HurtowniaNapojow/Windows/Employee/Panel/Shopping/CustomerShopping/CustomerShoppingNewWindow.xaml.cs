f50b5bbfac792c388bc54d8ebea5bbd6bf2daa0f
adasm <adasm@live.com>
2014-01-04 21:53:27 +0100

    Project Update, CustomerShoppingNewWindow

---
+++ HurtowniaNapojow/Windows/Employee/Panel/Shopping/CustomerShopping/CustomerShoppingNewWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using HurtowniaNapojow.Helpers;
+using HurtowniaNapojow.Database;
+using HurtowniaNapojow.Database.HurtowniaNapojowDataSetTableAdapters;
+
+namespace HurtowniaNapojow.Windows.Employee.Panel.Shopping.CustomerShopping
+{
+    /// <summary>
+    /// Interaction logic for EmployeeNewWindow.xaml
+    /// </summary>
+    public partial class CustomerShoppingNewWindow
+    {
+        private readonly ShoppingWindow _shoppingWindow;
+
+        public CustomerShoppingNewWindow(ShoppingWindow shoppingWindow)
+        {
+            InitializeComponent();
+            _shoppingWindow = shoppingWindow;
+            CustomersDataGrid.RebindContext(new KlienciTableAdapter().GetData());
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void CustomersChoose_Clicked(object sender, RoutedEventArgs e)
+        {
+            var customerRow = (((DataRowView)((Button)sender).DataContext)).Row as HurtowniaNapojowDataSet.KlienciRow;
+            if (customerRow == null) return;
+            
+            HurtowniaNapojowDataSet.ZakupyKlientaRow shopingRow = DataBaseShoppingHelper.AddNewShopping(SessionHelper.Instance.CurrentEmployee, customerRow);
+            (new CustomerShoppingDetailsWindow(_shoppingWindow, new EmployeeShopping(shopingRow))).ShowDialog();
+            _shoppingWindow.SetShoppingBinding();
+            Close();
+        }
+    }
+}
