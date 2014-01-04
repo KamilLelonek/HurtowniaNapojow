33bf440b845cc80ad26f9ebf44fa9f56b25cd946
adasm <adasm@live.com>
2014-01-04 18:05:47 +0100

    Added Drink Details Helpers

---
+++ HurtowniaNapojow/Helpers/DataBaseTasteHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HurtowniaNapojow.Database;
+using HurtowniaNapojow.Database.HurtowniaNapojowDataSetTableAdapters;
+
+namespace HurtowniaNapojow.Helpers
+{
+    public static class DataBaseTasteHelper
+    {
+        private static readonly SmakiTableAdapter TasteTableAdapter = new SmakiTableAdapter();
+        private static IEnumerable<HurtowniaNapojowDataSet.SmakiRow> _tastesData = TasteTableAdapter.GetData();
+
+        public static IEnumerable<HurtowniaNapojowDataSet.SmakiRow> GetTastesData()
+        {
+            return _tastesData;
+        }
+
+        public static HurtowniaNapojowDataSet.SmakiRow GetTasteByID(int tasteId)
+        {
+            return GetTastesData().First(taste => taste.Identyfikator == tasteId);
+        }
+    }
+}
