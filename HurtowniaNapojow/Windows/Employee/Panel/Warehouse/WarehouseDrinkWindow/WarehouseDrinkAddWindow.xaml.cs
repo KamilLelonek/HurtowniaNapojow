2c8a7d37a2ac182fc7b5066e79c43f5d89be8ab1
jaroslav91 <jaroslav91@gmail.com>
2014-01-11 17:25:20 +0100

    Added Missing files

---
+++ HurtowniaNapojow/Windows/Employee/Panel/Warehouse/WarehouseDrinkWindow/WarehouseDrinkAddWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using HurtowniaNapojow.Database;
+using HurtowniaNapojow.Helpers;
+using HurtowniaNapojow.Utils;
+
+
+namespace HurtowniaNapojow.Windows.Employee.Warehouse.WarehouseDrinkWindow
+{
+    /// <summary>
+    /// Interaction logic for WarehouseDrinkAddWindow.xaml
+    /// </summary>
+    public partial class WarehouseDrinkAddWindow
+    {
+        private ProducerDrinkHelper _producerDrink;
+        private WarehouseDrink _warehouseProduct;
+        private readonly Validator _validator = Validator.Instance;
+
+
+        public WarehouseDrinkAddWindow(ref HurtowniaNapojowDataSet.NapojeProducentaRow productRow)
+        {
+            InitializeComponent();
+            _producerDrink = new ProducerDrinkHelper(productRow);
+            _warehouseProduct = new WarehouseDrink();
+            ExpiryDate.SelectedDate = DateTime.Now;
+            SetBinding();
+        }
+
+
+        private void SetBinding()
+        {
+            DrinkDetailsGrid.DataContext = _producerDrink;
+        }
+
+        private void AddButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (_validator.AreControlsEmpty(ProductPrice, ProductAmount, Location )) return;
+            int amount = 0;
+            float price = _producerDrink.Price*(float)1.30;
+            bool validAmount = int.TryParse(ProductAmount.Text, out amount);
+            bool validPrice = float.TryParse(ProductPrice.Text, out price);
+            if (!validAmount || !validPrice) return;
+           
+            var idProducerDrink = _producerDrink.Id;
+            var quantity = amount;
+            var warehousePrice = price;
+            var location = Location.Text;
+            var expirationDate = ExpiryDate.SelectedDate.Value;
+            //var expirationDate = (DateTime)ExpiryDate.DataContext;
+            DataBaseWarehouseDrinkHelper.AddNewWarehouseDrink(idProducerDrink,quantity,warehousePrice,location,expirationDate);
+        }
+
+      
+
+        private void CloseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ProductAmountDecrease_OnClick(object sender, RoutedEventArgs e)
+        {
+            int amount = 0;
+            bool validAmount = int.TryParse(ProductAmount.Text, out amount);
+            if (validAmount && amount > 0) amount -= 1;
+            else amount = 0;
+
+            ProductAmount.Text = amount + "";
+           
+        }
+
+        private void ProductAmountIncrease_OnClick(object sender, RoutedEventArgs e)
+        {
+            int amount = 0;
+            bool validAmount = int.TryParse(ProductAmount.Text, out amount);
+            if (validAmount) amount += 1;
+            else amount = 1;
+
+            ProductAmount.Text = amount + "";
+            
+        }
+
+       
+    }
+}
---
+++ HurtowniaNapojow/Windows/Employee/Panel/Warehouse/WarehouseDrinkWindow/WarehouseDrinkNewWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HurtowniaNapojow.Database;
+using System.Windows;
+using System.Windows.Controls;
+using HurtowniaNapojow.Helpers;
+using HurtowniaNapojow.Reports.Admin;
+using HurtowniaNapojow.Utils;
+
+namespace HurtowniaNapojow.Windows.Employee.Warehouse.WarehouseDrinkWindow
+{
+    /// <summary>
+    /// Interaction logic for EmployeeNewWindow.xaml
+    /// </summary>
+    public partial class WarehouseDrinkNewWindow
+    {
+        private readonly Validator _validator = Validator.Instance;
+
+        public WarehouseDrinkNewWindow()
+        {
+           InitializeComponent();
+            DrinkDataGrid.DataContext = ProducerDrinkHelper.GetProducerDrinksLinq().OrderBy(d => d.Name);
+            SetDrinkComponentsEvents();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            CloseButton.PerformClick();
+        }
+
+        private void DrinkChoose_Clicked(object sender, RoutedEventArgs e)
+        {
+            var drinkRow = (((ProducerDrinkHelper)((Button)sender).DataContext));
+            if (drinkRow == null) return;
+
+            HurtowniaNapojowDataSet.NapojeProducentaRow productRow = DataBaseProducerDrinkHelper.GetDrinkByID(drinkRow.Id);
+            (new WarehouseDrinkAddWindow( ref productRow)).ShowDialog();
+            
+            Close();
+        }
+
+        private void SetDrinkComponentsEvents()
+        {
+            DrinkFilterTextBox.TextChanged += (sender, args) => DrinkFilterChanged();
+            DrinkFilterComboBox.SelectionChanged += (sender, args) => DrinkFilterChanged();
+        }
+        private void DrinkResetFilterButton_Click(object sender, RoutedEventArgs e)
+        {
+            DrinkFilterComboBox.Text = Globals.FILTER_SELECT;
+            DrinkFilterTextBox.Text = "";
+            DrinkDataGrid.DataContext = ProducerDrinkHelper.GetProducerDrinks();
+        }
+        private void DrinkFilterChanged()
+        {
+            var comboBoxItem = DrinkFilterComboBox.SelectedItem as ComboBoxItem;
+            if (comboBoxItem == null) DrinkFilterComboBox.SelectedIndex = 0;
+            comboBoxItem = DrinkFilterComboBox.SelectedItem as ComboBoxItem;
+
+            var filterType = comboBoxItem.Name.ToString();
+            var filterValue = DrinkFilterTextBox.Text.ToLower();
+            if (filterType == "Nazwa") DrinkDataGrid.DataContext = (ProducerDrinkHelper.GetProducerDrinks()).Where(d => d.Name.ToLower().Contains(filterValue)).OrderBy(d => d.Name);
+            else if (filterType == "Smak") DrinkDataGrid.DataContext = (ProducerDrinkHelper.GetProducerDrinks()).Where(d => d.TasteName.ToLower().Contains(filterValue)).OrderBy(d => d.Name);
+            else if (filterType == "Gaz") DrinkDataGrid.DataContext = (ProducerDrinkHelper.GetProducerDrinks()).Where(d => d.GasName.ToLower().Contains(filterValue)).OrderBy(d => d.Name);
+            else if (filterType == "Producent") DrinkDataGrid.DataContext = (ProducerDrinkHelper.GetProducerDrinks()).Where(d => d.ProducerName.ToLower().Contains(filterValue)).OrderBy(d => d.Name);
+            else if (filterType == "Typ") DrinkDataGrid.DataContext = (ProducerDrinkHelper.GetProducerDrinks()).Where(d => d.TypeName.ToLower().Contains(filterValue)).OrderBy(d => d.Name);
+            else if (filterType == "NazwaSztuki") DrinkDataGrid.DataContext = (ProducerDrinkHelper.GetProducerDrinks()).Where(d => d.PiecePackageName.ToLower().Contains(filterValue)).OrderBy(d => d.Name);
+            else if (filterType == "PojSztuki") DrinkDataGrid.DataContext = (ProducerDrinkHelper.GetProducerDrinks()).Where(d => d.PiecePackageVolume.ToString().Contains(filterValue)).OrderBy(d => d.Name);
+            else if (filterType == "NazwaZbiorczego") DrinkDataGrid.DataContext = (ProducerDrinkHelper.GetProducerDrinks()).Where(d => d.BulkPackageName.ToLower().Contains(filterValue)).OrderBy(d => d.Name);
+            else if (filterType == "PojZbiorczego") DrinkDataGrid.DataContext = (ProducerDrinkHelper.GetProducerDrinks()).Where(d => d.BulkPackageVolume.ToString().Contains(filterValue)).OrderBy(d => d.Name);
+            else DrinkDataGrid.DataContext = ProducerDrinkHelper.GetProducerDrinks().OrderBy(d => d.Name);
+        }
+    }
+}
